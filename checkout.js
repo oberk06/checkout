2ac0b7c7457599adaa74418201327dfb30f515df
oberk06 <oguzhanberk174@gmail.com>
2023-06-01 01:25:50 +0300

    isim

--- checkout.js
+++ checkout.js
@@ -1,12 +1,12 @@
-// const taxRate = 0.18;
-// const shippingPrice = 15;
-// const shippingFreePrice = 300;
+const taxRate = 0.18;
+const shippingPrice = 15;
+const shippingFreePrice = 300;
 
 window.addEventListener("load", () => {
   allproduct();
-  // localStorage.setItem("taxRate", taxRate);
-  // localStorage.setItem("shippingPrice", shippingPrice);
-  // localStorage.setItem("shippingFreePrice", shippingFreePrice);
+  localStorage.setItem("taxRate", taxRate);
+  localStorage.setItem("shippingPrice", shippingPrice);
+  localStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
 
 const container = document.getElementById("container");
